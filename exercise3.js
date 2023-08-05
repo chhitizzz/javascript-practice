bb38ff9d7d3081def553815e0a3438b950dc7613
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-05 12:38:22 +0545

    3

--- exercise3.js
+++ exercise3.js
@@ -1,2 +1,9 @@
 // Function that takes an array of numbers as input and returns the sum of all the numbers in the array
 
+function arraySum(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total;
+}
