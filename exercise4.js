9dc24bd9d2968a37345260516100a8d62ac75b0e
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-06 10:25:11 +0545

    4

--- exercise4.js
+++ exercise4.js
@@ -1,1 +1,11 @@
-// Function that takes a number as input and checks if it is positive, negative, or zero. Return "Positive," "Negative," or "Zero" accordingly.
+// Function that takes a number as input and checks if it is positive, negative, or zero. Return "Positive," "Negative," or "Zero" accordingly
+
+function checkNumber(number) {
+    if (number > 0) {
+        return 'The number is positive.';
+    } else if (number == 0) {
+        return 'The number is zero.';
+    } else {
+        return 'The number is negative.';
+    }
+}
