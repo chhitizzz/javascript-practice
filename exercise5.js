cc8e6ab5d4f562325f32adea414fd424e942e17e
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-07 09:06:03 +0545

    5

--- exercise5.js
+++ exercise5.js
@@ -1,1 +1,9 @@
 // Function that takes two numbers as input and returns the larger of the two
+
+function largerNumber(x, y) {
+    if (x > y) {
+        return 'The larger number is ' + x;
+    } else {
+        return 'The larger number is ' + y;
+    }
+}
