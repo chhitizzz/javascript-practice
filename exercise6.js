0307008d6e82abcedd7d052fd6eb75787e9b4655
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-08 10:11:26 +0545

    6

--- exercise6.js
+++ exercise6.js
@@ -1,1 +1,5 @@
 // Function that takes a string as input and checks if it contains the word "JavaScript". Return true if it does, false otherwise.
+
+function containsJavaScript(inputString) {
+    inputString.includes("JavaScript");
+}
