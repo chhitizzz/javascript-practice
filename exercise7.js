2fe431123692e667338c4ae519865bffb1f0c247
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-09 09:57:56 +0545

    7

--- exercise7.js
+++ exercise7.js
@@ -1,1 +1,9 @@
 // Function that takes a number as input and checks if it is divisible by both 3 and 5. Return true if it is, false otherwise
+
+function divisible(number) {
+    if (number % 3 && number % 5 == 0) {
+        return number + "is divisible by both 3 and 5";
+    }
+}
+
+console.log(divisible(15));
