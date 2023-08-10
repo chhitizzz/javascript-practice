3c70459e07163c9d3ed0cf091d51532d03a3e94e
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-10 09:25:09 +0545

    8

--- exercise8.js
+++ exercise8.js
@@ -1,1 +1,9 @@
 // Function that takes a number as input and checks if it is even. If it's even, return "Even," otherwise return "Odd".
+
+function evenOddComparison(number) {
+    if (number % 2 === 0) {
+        return 'The number is even.';
+    } else {
+        return 'The number is odd.';
+    }
+} 
