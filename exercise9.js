2b0d88704efd1aff0438900e999712910877c401
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-11 10:15:34 +0545

    9

--- exercise9.js
+++ exercise9.js
@@ -4,5 +4,5 @@ function equalStrings(str1, str2) {
     return str1 === str2;
 }
 
-console.log(equalStrings('apple', 'apple'));
-console.log(equalStrings('apple', 'banana'));
+console.log(equalStrings('apple', 'apple')); // Output: true
+console.log(equalStrings('orange', 'banana')); // Output: false
