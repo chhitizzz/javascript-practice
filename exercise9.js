bf1e94c96db18f6269645ced48f97dee5fd54dd2
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-11 10:13:51 +0545

    9

--- exercise9.js
+++ exercise9.js
@@ -1,1 +1,8 @@
 // Function that takes two strings as input and checks if they are equal. Return true if they are equal, false otherwise
+
+function equalStrings(str1, str2) {
+    return str1 === str2;
+}
+
+console.log(equalStrings(apple, apple));
+console.log(equalStrings(apple, banana));
