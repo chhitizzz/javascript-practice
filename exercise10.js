dc737e6397b9398b2faa81deddb5feb3749dd306
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-12 20:59:22 +0545

    10

---
+++ exercise10.js
@@ -0,0 +1,1 @@
+// 10. Function that takes a number as input and returns "Fizz" if the number is divisible by 3, "Buzz" if it's divisible by 5, and "FizzBuzz" if it's divisible by both 3 and 5. Otherwise, return the number itself
