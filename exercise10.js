bf9f930845b3cfd91bc1848cd7fd1b6f20a6ff5e
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-12 21:02:46 +0545

    10

--- exercise10.js
+++ exercise10.js
@@ -1,1 +1,13 @@
-// 10. Function that takes a number as input and returns "Fizz" if the number is divisible by 3, "Buzz" if it's divisible by 5, and "FizzBuzz" if it's divisible by both 3 and 5. Otherwise, return the number itself
+// Function that takes a number as input and returns "Fizz" if the number is divisible by 3, "Buzz" if it's divisible by 5, and "FizzBuzz" if it's divisible by both 3 and 5. Otherwise, return the number itself
+
+function divisibleNumber(number) {
+    if (number % 3 === 0) {
+        return 'Fizz';
+    } else if (number % 5 === 0) {
+        return 'Buzz';
+    } else if (number % 3 && number % 5 === 0) {
+        return 'FizzBuzz';
+    } else {
+        return number;
+    }
+}
