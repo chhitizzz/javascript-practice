f920e0537348d6d44d149f88aee735f6a0339455
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-13 20:43:10 +0545

    11

--- exercise11.js
+++ exercise11.js
@@ -1,1 +1,11 @@
 // Function that takes a year as input and checks if it's a leap year. Return true if it's a leap year, false otherwise
+
+function checkLeapYear(year) {
+    if (year % 400 === 0 && year % 100 === 0) {
+        return year + ' is a leap year.';
+    } else if (year % 4 === 0 && year % 100 !== 0) {
+        return year + ' is a leap year.';
+    } else {
+        return year + ' is not a leap year.';
+    }
+}
