d667a5b3d974f677d24174560aa7594cddcef1a0
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-14 21:34:04 +0545

    12

--- exercise12.js
+++ exercise12.js
@@ -1,1 +1,23 @@
 // Function that takes an array of numbers as input and returns the number of positive, negative, and zero values in the array as an object
+
+function countPositiveNegativeZero(numbers) {
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+
+    for(let i = 0; i < numbers.length; i++) {
+        if(numbers[i] > 0) {
+            positive++;
+        } else if (numbers[i] < 0) {
+            negative++;
+        } else {
+            zero++;
+        }
+    }
+
+    return {
+        positive: positive,
+        negative: negative,
+        zero: zero
+    };
+}
