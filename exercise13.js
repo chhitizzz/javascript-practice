541551752e94ea520553a79c43352a8e14832808
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-15 21:26:20 +0545

    13

--- exercise13.js
+++ exercise13.js
@@ -1,1 +1,11 @@
 // Function that takes three numbers as input and returns the largest of the three without using the built-in `Math.max()` method
+
+function largestOfThreeNumbers(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1 + ' is the largest number.';
+    } else if (num2 > num1 && num2 > num3) {
+        return num2 + ' is the largest number.';
+    } else {
+        return num3 + ' is the largest number.';
+    }
+}
