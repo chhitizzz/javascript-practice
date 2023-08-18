385b796fcfdfc861ffd1f369dd666c07462916e8
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-18 12:18:56 +0545

    16

--- exercise16.js
+++ exercise16.js
@@ -1,1 +1,8 @@
 // Function that takes a number as input and prints a multiplication table for that number up to 10
+
+function multiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        result = number * i;
+        console.log(`${number} x ${i} = ${result}`);
+    }
+}
