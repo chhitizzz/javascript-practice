8ca6d9e14723723fe86a989ff3e184adff0b1d29
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-19 16:18:01 +0545

    17

--- exercise17.js
+++ exercise17.js
@@ -1,1 +1,9 @@
 // Function that takes an array of numbers as input and calculates the sum of all the numbers using a loop
+
+function calculateSum(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.lenght; i++) {
+        sum += numbers;
+    }
+    return sum; 
+}
