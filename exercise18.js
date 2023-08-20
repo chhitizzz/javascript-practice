5fa52850f03132f8e10155848e4b6f6bd5409deb
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-20 21:31:21 +0545

    18

--- exercise18.js
+++ exercise18.js
@@ -1,1 +1,7 @@
 // Program that iterates over an array of strings and prints each string to the console
+
+const stringArray = ['Hello', 'World', 'JavaScript', 'Python'];
+
+for (let i = 0; i < stringArray.length; i++) {
+    console.log(stringArray[i]);
+}
