4e73072e9cab17af1c070bed5c1a7c3aff03928d
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-21 21:19:09 +0545

    19

--- exercise19.js
+++ exercise19.js
@@ -1,1 +1,7 @@
 // Function that takes a number as input and checks if it's a prime number using a loop. Return true if it's prime, false otherwise
+
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+}
