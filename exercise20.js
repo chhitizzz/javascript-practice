0302de888a3a248a5e170d6ede250c96bb1133b8
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-22 21:26:25 +0545

    20

--- exercise20.js
+++ exercise20.js
@@ -1,1 +1,7 @@
 // Program that iterates over an array of objects representing people (with name and age properties) and prints their names to the console
+
+const peopleArray = [
+    {name: "Rashford", age: 25},
+    {name: "Bruno", age: 28},
+    {name: "Varane", age: 30},
+];
