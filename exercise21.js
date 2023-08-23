5ce1c835431765e753f241cee666129807ca5430
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-23 21:26:10 +0545

    21

--- exercise21.js
+++ exercise21.js
@@ -1,1 +1,13 @@
 // Function that takes a string as input and counts the number of occurrences of a specific character using a loop
+
+function stringOccurrence(inputString, targetCharacter) {
+    let count = 0;
+
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] === targetCharacter) {
+            count++;
+        }
+    }
+
+    return count;
+}
