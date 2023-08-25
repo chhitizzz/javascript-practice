aa8f0fc1827056a26da0be979514edef9e95474b
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-25 21:04:23 +0545

    23

--- exercise23.js
+++ exercise23.js
@@ -1,1 +1,11 @@
 // Function that takes a positive integer as input and prints a right-angled triangle of stars (*) with the given height
+
+function printTriangle(height) {
+    for (let i = 1; i <= height; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += '*';
+        }
+        console.log(row);
+    }
+}
