b1f867f9dab7c846ff0036e94f83f46d1f88e6bd
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-26 21:30:57 +0545

    24

--- exercise24.js
+++ exercise24.js
@@ -1,1 +1,9 @@
-// Function that takes an array of numbers as input and returns the sum of all the numbers
+// Function that takes an array of numbers as input and returns the product of all the numbers
+
+function arrayProduct(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total *= numbers[i];
+    }
+    return total;
+}
