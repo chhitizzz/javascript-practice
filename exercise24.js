10118693d7a3bb695172d58f6dea8745dedefc2c
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-26 21:32:47 +0545

    24

--- exercise24.js
+++ exercise24.js
@@ -1,9 +1,13 @@
 // Function that takes an array of numbers as input and returns the product of all the numbers
 
 function arrayProduct(numbers) {
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
+    let total = 1;
+    for (let i = 1; i < numbers.length; i++) {
         total *= numbers[i];
     }
     return total;
 }
+
+const numberArray = [1, 2, 3, 4, 5, 6];
+const product = arrayProduct(numberArray);
+console.log(product); 
