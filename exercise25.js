7a2b76d508ac54c9930eeacff9366ef2b8e78458
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-27 21:16:56 +0545

    25

--- exercise25.js
+++ exercise25.js
@@ -1,1 +1,10 @@
 // Function that takes an array of strings as input and returns a new array with all the strings in uppercase
+
+function arrayUppercase(strings) {
+    const upperCaseArray = [];
+
+    for (let i = 0; i < strings.length; i++) {
+        upperCaseArray.push(strings[i].toUpperCase());
+    }
+    return upperCaseArray;
+}
