01de12b57b76cf51d1e76a00bbb10ad59e1eeb89
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-28 21:58:31 +0545

    26

--- exericse26.js
+++ exericse26.js
@@ -1,1 +1,14 @@
 // Function that takes an array of numbers as input and returns a new array with only the even numbers
+
+function evenNumbers(numbersArray) {
+    const evenNumbersArray = [];
+
+    for (let i = 0; i < numbersArray.length; i++) {
+        if (numbersArray[i] % 2 === 0) {
+            evenNumbersArray.push(numbersArray[i]);
+        }
+    }
+
+    return evenNumbersArray
+}
+
