1c7694c9eb9d61afd7a1a35383e25a046a729a52
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-29 21:10:58 +0545

    27

--- exercise27.js
+++ exercise27.js
@@ -1,1 +1,12 @@
 // Function that takes an array of strings as input and returns the number of strings that have more than five characters
+
+function stringsMoreThanFiveChars(stringArray) {
+    let count = 0;
+
+    for (let i = 0; i < stringArray.length; i++) {
+        if (stringArray[i].length > 5) {
+            count ++;
+        }
+    }
+    return count; 
+}
