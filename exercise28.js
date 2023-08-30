bfbda33cf0ed65fee39ff2f6b266760e716d869b
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-30 14:10:31 +0545

    28

--- exercise28.js
+++ exercise28.js
@@ -1,1 +1,12 @@
 // Function that takes an array of numbers as input and returns the largest number in the array
+
+function largestNumber(numberArray) {
+    let largest = numberArray[0];
+
+    for(let i = 1;i < numberArray.length; i++) {
+        if(numberArray[i] > largest) {
+            largest = numberArray[i];
+        }
+    }
+    return largest;
+}
