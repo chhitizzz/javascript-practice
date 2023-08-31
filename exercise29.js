e994885fa6902da4a3527575c55e8e172d5f49b5
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-31 22:19:12 +0545

    29

--- exercise29.js
+++ exercise29.js
@@ -1,1 +1,6 @@
 // Function that takes an array of strings as input and returns a new array with the strings sorted in alphabetical order
+
+function alphabeticalOrderStrings(stringsArray) {
+    const sortedArray = stringsArray.slice().sort();
+    return sortedArray
+}
