7ae73d9315321ca39c1b41a79bdf3d3bf17f5c01
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-01 21:21:25 +0545

    30

--- exercise30.js
+++ exercise30.js
@@ -1,1 +1,7 @@
 // Function that takes an array of numbers as input and returns the average of those numbers
+
+function calculateAverage(numbersArray) {
+    if (numbersArray === 0) {
+        return 0;
+    }
+}
