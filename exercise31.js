41a962c313c269544c74aef94fea978fc124fc38
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-02 20:38:37 +0545

    31

--- exercise31.js
+++ exercise31.js
@@ -1,1 +1,13 @@
 // Function that takes an array of strings as input and returns a new array with the strings reversed
+
+function reverseStrings(stringsArray) {
+    const reverseArray = [];
+
+    for(let i = 0; i < stringsArray.length; i++) {
+        const originalString = stringsArray[i];
+        const reverseString = originalString.split('').reverse('').join('');
+        reverseArray.push(reverseArray);
+    }
+
+    return reverseArray;
+}
