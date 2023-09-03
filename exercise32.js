f8ef67c172d92fef6126545396891645fb33c62b
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-03 21:24:40 +0545

    32

--- exercise32.js
+++ exercise32.js
@@ -7,4 +7,4 @@ function combineArray(array1, array2) {
 const array1 = [10, 20, 30];
 const array2 = [40, 50, 60];
 const combinedArray = combineArray(array1, array2);
-console.log(combinedArray);
+console.log(combinedArray); // Output: [ 10, 20, 30, 40, 50, 60 ]
