b2327e236a188e7da6a7acfd9a90db535b1328ad
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-03 21:22:51 +0545

    32

--- exercise32.js
+++ exercise32.js
@@ -1,1 +1,5 @@
 // Function that takes two arrays as input and returns a new array with the elements of both arrays combined
+
+function combineArray(array1, array2) {
+    return array1.concat(array2);
+}
