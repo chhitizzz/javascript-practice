9d096e592fda055af6b73bc747c192bad5851467
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-04 20:27:13 +0545

    33

--- exercise33.js
+++ exercise33.js
@@ -1,1 +1,9 @@
 // Function that takes an array of numbers as input and returns a new array with only unique values (no duplicates)
+
+function removeDuplicates(numbersArray) {
+    const uniqueNumbersSet = new Set(numbersArray);
+
+    const uniqueNumbersArray = Array.from(uniqueNumbersSet);
+
+    return uniqueNumbersArray;
+}
