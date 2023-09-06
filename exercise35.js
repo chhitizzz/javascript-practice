df75d9cc9394db1e9dec02da4a95a0fe4adc1db0
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-06 21:40:32 +0545

    35

--- exercise35.js
+++ exercise35.js
@@ -8,3 +8,7 @@ function personObject(person) {
         console.log('Invalid person object. Make sure it has name and age properties.');
     }
 }
+
+const person1 = {name: 'Nivia', age: 20};
+const person2 = {name: 'Nivi', age: 21};
+const invalidInput = { };
