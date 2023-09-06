5ab249238f723464444727b4f4093880ba9846e0
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-06 21:39:29 +0545

    35

--- exercise35.js
+++ exercise35.js
@@ -1,1 +1,10 @@
 // Function that takes an object (representing a person) as input and prints their name and age to the console
+
+function personObject(person) {
+    if (person && person.name && person.age) {
+        console.log('Name: ${person.name}');
+        console.log('Age: ${person.age}');
+    } else {
+        console.log('Invalid person object. Make sure it has name and age properties.');
+    }
+}
