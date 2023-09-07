bcdd374e4885a38d4489ca2cc07590e53ce99f7f
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-07 21:56:28 +0545

    36

--- exercise36.js
+++ exercise36.js
@@ -1,1 +1,20 @@
-// Create an array of objects, each representing a book with properties: title, author, and year.
+// Create an array of objects, each representing a book with properties: title, author, and year
+
+const booksArray = [
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      year: 1951
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      year: 1960
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      year: 1949
+    }
+  ];
+  
