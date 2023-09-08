0d0aa30eb38085bbdceb2f72c7609b970facd042
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-08 20:59:54 +0545

    37

--- exercise37.js
+++ exercise37.js
@@ -1,1 +1,20 @@
 // Function that takes an array of book objects as input and returns an array with only the book titles
+
+const booksArray = [
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      year: 1951
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      year: 1960
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      year: 1949
+    }
+  ];
+  
