66662d80406b52258ee410ea5b025046aed7dae1
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-09 21:08:20 +0545

    38

--- exercise38.js
+++ exercise38.js
@@ -1,1 +1,19 @@
-// unction that takes an array of book objects as input and returns the total number of years (sum of years) for all books
+// Function that takes an array of book objects as input and returns the total number of years (sum of years) for all books
+
+const booksArray = [
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      year: 1951
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      year: 1960
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      year: 1949
+    }
+  ];
