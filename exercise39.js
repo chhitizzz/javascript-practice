bc2a5ad5422dd3b6956bebfd58a340446c9b8b95
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-10 11:58:32 +0545

    39

--- exercise39.js
+++ exercise39.js
@@ -3,5 +3,9 @@
 const car = {
     make: 'BMW',
     model: 'i5 Sedan',
-    year: '2024'   
+    year: 2024   
 }
+
+console.log(car.make); // Output: BMW
+console.log(car.model); // Output: i5 Sedan
+console.log(car.year); // Output: 2024
