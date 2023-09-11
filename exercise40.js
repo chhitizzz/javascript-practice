6510a2eca81c2153039cc721472a832a80110a9d
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-11 12:04:55 +0545

    40

--- exercise40.js
+++ exercise40.js
@@ -1,1 +1,5 @@
 // Function that takes a car object as input and returns a string with the make, model, and year combined (e.g., "Toyota Corolla 2022")
+
+function carDetails (car) {
+    return `${car.make} ${car.model} ${car.year}`;
+}
