793b52b8b235ec11878cce266a5add60eba15764
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-12 22:02:02 +0545

    41

--- exercise41.js
+++ exercise41.js
@@ -1,1 +1,14 @@
 // Function that takes an array of car objects as input and returns the number of cars that are from a specific year
+
+function countCarsByYear(carsArray, targetYear) {
+    let count = 0;
+  
+    for (let i = 0; i < carsArray.length; i++) {
+      if (carsArray[i].year === targetYear) {
+        count++;
+      }
+    }
+  
+    return count;
+  }
+  
