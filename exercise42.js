a0c4b153de7016c95a45981ce64418fa1640a373
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-13 13:03:42 +0545

    42

--- exercise42.js
+++ exercise42.js
@@ -1,1 +1,11 @@
 // Create an object representing a student with properties: name, age, and grades (an array of numbers)
+
+const student = {
+    name: 'Subhu',
+    age: 21,
+    grades: [50, 60, 70, 80, 90]
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.grades);
