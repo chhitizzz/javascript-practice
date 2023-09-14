5ad426ef25e891acfe13bd5b64c8c7461b562aa1
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-14 10:19:00 +0545

    43

--- exercise43.js
+++ exercise43.js
@@ -1,1 +1,13 @@
 // Function that takes a student object as input and returns their average grade
+
+function averageGrade(student) {
+    const grades = student.grades;
+    if (grades.length === 0) {
+        return 0;
+    }
+
+    const sum = grades.reduce((acc, grades) => acc + grades, 0);
+    const average = sum / grades.length;
+
+    return average;
+}
