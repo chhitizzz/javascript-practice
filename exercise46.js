1db3dd7e4b07fe8817784786ed456bd937ddd8ae
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-17 20:20:46 +0545

    46

--- exercise46.js
+++ exercise46.js
@@ -1,1 +1,12 @@
 // Function that reverses a given string. For example, "hello" should return "olleh"
+
+function reverseString(str) {
+
+    let splitStr = str.split("");
+
+    let reverseStr = splitStr.reverse();
+
+    let joinStr = reverseStr.join("")
+
+    return joinStr;
+}
