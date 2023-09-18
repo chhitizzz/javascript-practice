1b9cbb0782a0710763ba1294eaa3ddf87ba03f72
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-18 21:18:05 +0545

    47

--- exercise47.js
+++ exercise47.js
@@ -1,1 +1,15 @@
 // Function to calculate the factorial of a given non-negative integer. The factorial of a non-negative integer `n` is the product of all positive integers less than or equal to `n`
+
+function calculateFactorial(number) {
+    if (number < 0) {
+        return "Number cannot be negative";
+    } else if (number === 0 || number === 1) {
+        return 1;
+    } else {
+        let result = 1;
+        for (let i = 2; i <=number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
