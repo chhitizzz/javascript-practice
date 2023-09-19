d6ff2fd62f0e96355d44b415af263fe049e7429b
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-19 20:22:40 +0545

    48

--- exercise48.js
+++ exercise48.js
@@ -1,1 +1,9 @@
 // Function that checks if a given word is a palindrome (reads the same forwards and backwards)
+
+function palindromeCheck (word) {
+    const newWord = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+    const reverseWord = newWord.split('').reverse().join('');
+
+    return newWord === reverseWord;
+}
