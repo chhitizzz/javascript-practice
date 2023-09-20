3c97f0da1225de65a65fa1e3c08a67a2e378f531
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-20 21:03:20 +0545

    49

--- exercise49.js
+++ exercise49.js
@@ -1,1 +1,14 @@
 // Function that counts the number of vowels in each string
+
+function countVowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+
+    for(let i = 0; i < str.lenght; i++) {
+        if(vowels.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
