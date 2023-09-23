45746bd92afa28db1b368538fc3242b5e24974ff
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-23 21:20:54 +0545

    52

--- exercise52.js
+++ exercise52.js
@@ -1,1 +1,11 @@
 // Function that takes an array of numbers and returns the sum of all the elements
+
+function arraySum(numberArray) {
+    let sum = 0;
+
+    for(let i = 0; i < numberArray.length; i++) {
+        sum += numberArray[i];
+    }
+
+    return sum;
+}
