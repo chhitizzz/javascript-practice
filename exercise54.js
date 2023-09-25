96984e915f6625056c7f13c01967ea6b8ca1a131
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-25 21:40:13 +0545

    54

--- exercise54.js
+++ exercise54.js
@@ -1,1 +1,17 @@
 // Function that converts the first letter of each word in a sentence to uppercase
+
+function capitalizeFirstLetterOfEachWord(sentence) {
+    const words = sentence.split(' ');
+    const capitalizedWords = [];
+  
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      capitalizedWords.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+    }
+  
+    return capitalizedWords.join(' ');
+  }
+  
+const sentence = "hello world! how are you?";
+const capitalizedSentence = capitalizeFirstLetterOfEachWord(sentence);
+console.log(capitalizedSentence); 
