10d4b2728b1747310090daf6c6fac2f6f14edab4
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-26 21:21:11 +0545

    55

--- exercise55.js
+++ exercise55.js
@@ -6,4 +6,4 @@ function reverseArray(arr) {
 
 const myArray = [10, 20, 30, 40, 50];
 const reversedArray = reverseArray(myArray);
-console.log(reversedArray);
+console.log(reversedArray); // Output: [ 50, 40, 30, 20, 10 ]
