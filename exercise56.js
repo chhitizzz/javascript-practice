c441c2d99f078c238a904fe7ce4d253dc8389c24
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-27 21:29:16 +0545

    56

--- exercise56.js
+++ exercise56.js
@@ -1,1 +1,18 @@
 // Function that checks if a given number is prime
+
+function isPrime(number) {
+    if (number < 2) {
+      return false;
+    }
+  
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+console.log(isPrime(11));  // Output: true
+console.log(isPrime(16));  // Output: false
