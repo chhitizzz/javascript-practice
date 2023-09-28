32d089be6e891c8d593a01a60e1b929f25f281a9
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-28 21:26:53 +0545

    57

--- exercise57.js
+++ exercise57.js
@@ -1,1 +1,10 @@
 // Function that concatenates two arrays
+
+function concatenateArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+  }
+  
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const concatenatedArray = concatenateArrays(array1, array2);
+console.log(concatenatedArray);  // Output: [1, 2, 3, 4, 5, 6]
