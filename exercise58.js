cf2363c55e70fa0eb494bf3140cbd31e701443ea
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-29 20:54:33 +0545

    58

--- exercise58.js
+++ exercise58.js
@@ -1,1 +1,20 @@
 // Function that generates the nth number in the Fibonacci sequence
+
+function fibonacci(n) {
+    if (n <= 1) {
+      return n;
+    }
+  
+    let fibPrev = 0;
+    let fibCurrent = 1;
+  
+    for (let i = 2; i <= n; i++) {
+      const temp = fibCurrent;
+      fibCurrent = fibPrev + fibCurrent;
+      fibPrev = temp;
+    }
+  
+    return fibCurrent;
+  }
+  
+console.log(fibonacci(6));  // Output: 8 
